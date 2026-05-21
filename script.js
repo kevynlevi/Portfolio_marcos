caa16c4a9b77157b0b58a6b9216b277cdfcee05f
kevyn <kevynlevi0@gmail.com>
2026-05-20 22:40:00 -0300

    Atualiza seção "Hero" com animações e informações pessoais; adiciona seção "Projetos" com cards e estilos aprimorados.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ document.querySelectorAll("a").forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault();
   });
-});
+}); 
 
 function abrirWhatsApp() {
   window.open("https://api.whatsapp.com/send/?phone=5577981147949&text=Olá%2C+Marcos%21+Vi+seu+portfólio+e+tenho+interesse+no+seu+trabalho+de+edição+de+vídeo.+Gostaria+de+conversar+melhor+sobre+um+projeto.&type=phone_number&app_absent=0", "_blank"); 
 }
+window.addEventListener("scroll", () => {
+  document.querySelector("header").classList.toggle("scrolled", window.scrollY > 50);
+});
